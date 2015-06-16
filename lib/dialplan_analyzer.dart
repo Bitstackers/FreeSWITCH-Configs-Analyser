6de1a8c4ec0ec72add2e6258249969eceb945119
Thomas Pedersen <tp@adaheads.com>
2015-06-16 12:03:23 +0200

    Implemented two simple checks.

--- lib/dialplan_analyzer.dart
+++ lib/dialplan_analyzer.dart
@@ -1,5 +1,49 @@
 part of freeswitch_config_analyzer;
 
 void validateDialplans(String path) {
-  //TODO
+  Directory receptionsDirectory = new Directory(libpath.join(path, 'receptions'));
+  List<DialplanFile> receptionDialplans = new List<DialplanFile>();
+
+  for (FileSystemEntity entity in receptionsDirectory.listSync()) {
+    File file = entity as File;
+    if (file != null && file.path.endsWith('.xml')) {
+      String filename = libpath.basenameWithoutExtension(file.path);
+      String content = file.readAsStringSync();
+      XmlDocument tree = parse(content);
+      DialplanFile model = new DialplanFile.fromXml(tree);
+      receptionDialplans.add(model);
+      model.filePath = file.path;
+      _validateDialplanFile(model, filename);
+    }
+  }
+
+  //Look for multiple menus with the same name.
+  HashMap<String, String> menuNames = new HashMap<String, String>();
+  for(DialplanFile model in receptionDialplans) {
+    for(Extension extension in model.extensions) {
+      if(menuNames.containsKey(extension.name)) {
+        print('There exists multiple extensions with the name: "${extension.name}" File: "${model.filePath}" AND File: "${menuNames[extension.name]}"');
+      } else {
+        menuNames[extension.name] = model.filePath;
+      }
+    }
+  }
+
+  //Print Errors
+  for(DialplanFile model in receptionDialplans) {
+    if (model.allErrors.isNotEmpty) {
+      print('---- Errors in file: "${model.filePath}" ----');
+      model.allErrors.forEach(print);
+    }
+  }
+}
+
+void _validateDialplanFile(DialplanFile dialplan, String filename) {
+  //Check if the extension name matches the filename.
+  for(Extension extension in dialplan.extensions) {
+    if (!extension.name.startsWith(filename)) {
+      extension.errors.add('Extension name does not match file name. Extension: ${extension.name}');
+    }
+  }
+
 }
--- lib/model/dialplan_model.dart
+++ lib/model/dialplan_model.dart
@@ -1,10 +1,57 @@
 part of model;
 
 class DialplanFile {
-  String filepath;
+  String filePath;
+  List<Extension> extensions = new List<Extension>();
+  List<String> errors = new List<String>();
 
+  List<String> get allErrors {
+    var list = new List.from(errors);
+    for (Extension menu in extensions) {
+      list.addAll(menu.errors);
+    }
+
+    return list;
+  }
+
+  DialplanFile.fromXml(XmlDocument xml) {
+    //Checks there is only one top Element
+    if (xml.children.where(isNotComment).length != 1) {
+      errors.add('It should start with just one include node.');
+      return;
+    }
+
+    //Checks the first element is an include element
+    XmlElement includeNode = xml.children.where(isNotComment).first;
+    if (includeNode.name.toString() != 'include') {
+      errors.add(
+          'It should start with a "include" node. "${includeNode.name}" != "include"');
+      return;
+    }
+
+    for (XmlNode node in includeNode.children.where(isNotComment)) {
+      if (node is XmlElement) {
+        XmlElement menuElement = node;
+        String elementName = 'extension';
+        if (menuElement.name.toString() != elementName) {
+          errors.add('Inside its include node, should only be $elementName nodes. ${menuElement.name.toString()} != "$elementName"');
+        } else {
+          extensions.add(new Extension.fromXml(node));
+        }
+      } else if (node is XmlText && node.text.trim().length > 0) {
+        //This might happend if there are a line-break.
+        errors.add('Beside the menus are there other stuff than XmlElements Type:(${node.runtimeType}) Content:[${node}]');
+      }
+    }
+  }
 }
 
 class Extension {
+  List<String> errors = new List<String>();
+
   String name;
+
+  Extension.fromXml(XmlElement xml) {
+    this.name = extractAttributeValue(xml, 'name', errors, required: true);
+  }
 }
